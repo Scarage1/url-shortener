d800bb63dc12e52375c27e9815d48f83903f3d67
shivamkumar <shivamkr1321@gmail.com>
2026-07-03 20:21:21 +0530

    feat: add graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/Scarage1/url-shortener/internal/cache"
 	"github.com/Scarage1/url-shortener/internal/config"
 	"github.com/Scarage1/url-shortener/internal/database"
 	"github.com/Scarage1/url-shortener/internal/logger"
 	"github.com/Scarage1/url-shortener/internal/router"
+
 	"go.uber.org/zap"
 )
 
 func main() {
+
 	logger.InitLogger()
 	defer logger.Log.Sync()
 
@@ -26,13 +35,75 @@ func main() {
 		redisClient,
 	)
 
-	logger.Log.Info(
-		"Server started on port",
+	server := &http.Server{
+		Addr:    ":" + cfg.Port,
+		Handler: r,
+	}
 
-		zap.String(
-			"port",
-			cfg.Port,
-		),
+	// START SERVER FIRST
+	go func() {
+
+		logger.Log.Info(
+			"server started",
+			zap.String(
+				"port",
+				cfg.Port,
+			),
+		)
+
+		if err := server.ListenAndServe(); err != nil &&
+			err != http.ErrServerClosed {
+
+			logger.Log.Fatal(
+				"server failed",
+				zap.Error(err),
+			)
+		}
+	}()
+
+	// THEN WAIT FOR SHUTDOWN SIGNAL
+	quit := make(
+		chan os.Signal,
+		1,
 	)
-	r.Run(":" + cfg.Port)
+
+	signal.Notify(
+		quit,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+	)
+
+	<-quit
+
+	logger.Log.Info(
+		"shutting down server",
+	)
+
+	ctx, cancel :=
+		context.WithTimeout(
+			context.Background(),
+			5*time.Second,
+		)
+
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+
+		logger.Log.Fatal(
+			"forced shutdown",
+			zap.Error(err),
+		)
+	}
+
+	if err := redisClient.Close(); err != nil {
+
+		logger.Log.Error(
+			"redis close error",
+			zap.Error(err),
+		)
+	}
+
+	logger.Log.Info(
+		"server exited",
+	)
 }
